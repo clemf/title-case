031b94d2c71686383a9451f6b06343f5112f0fd2
Clem Freeman and Sean Holstrom <clemfree@gmail.com>
2015-02-10 09:47:11 -0800

    Add parsing for multiple word input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 function titleCase(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe("titleCase", function() {
   it("lowercases all non-first letters of a word", function() {
     expect(titleCase("CAT")).to.equal("Cat");
   });
+
+  it("parses multiple word input", function() {
+    expect(titleCase("CaT meOw")).to.equal("Cat Meow");
+  });
 });
