e1b3137948bc2d71b2729c1fb3478e2281a5277d
Clem Freeman and Sean Holstrom <basementbebop@gmail.com>
2015-02-10 09:41:25 -0800

    Add test for lowercasing other letters

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("titleCase", function() {
   it("capitalizes the first letter of a word", function() {
     expect(titleCase("cat")).to.equal("Cat");
   });
+
+  it("lowercases all non-first letters of a word", function() {
+    expect(titleCase("CAT")).to.equal("Cat");
+  });
 });
