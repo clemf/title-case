e1f96908498c13bc63f3bc1a339f88bebb373ffe
Clem Freeman and Sean Holstrom <basementbebop@gmail.com>
2015-02-10 10:39:19 -0800

    Add functionality to filter non desirable words

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
+var noCapsList = "a, an, the, at, by, for, in, of, on, to, up, and, as, but, it, or, nor";
 function titleCase(string) {
   var results = "";
   string.split(" ").forEach(function(word) {
-    results = results + (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) + " ";
+    if (noCapsList.indexOf(word) > -1) {
+      results = results + (word.toLowerCase()) + " ";
+    } else {
+      results = results + (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) + " ";
+    }
   });
   results = results.slice(0, -1)
   return results;
 }
-
-//a, an, the, at, by, for, in, of, on, to, up, and, as, but, it, or, and nor
